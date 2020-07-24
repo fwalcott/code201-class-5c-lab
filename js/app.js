4035094862f998950e8b117f53af1276dfb28040
Felicia Walcott <fwalcott.walcott@gmail.com>
2020-07-23 22:50:57 -0700

    added files

---
+++ js/app.js
@@ -0,0 +1,88 @@
+'use strict'  
+
+
+var userName = prompt('Hello, My name is Felicia. What is your name?'); 
+console.log('Hello' + userName + 'Welcome to my about me page!!!'); 
+alert('Hello' + userName + 'Welcome to my  about me page!!!'); 
+
+
+var questionOne = prompt(userName + 'Do I like spicy food? Yes or No'); 
+console.log(userName + 'Do I like spicy food? Yes or No');  
+questionOne.toLowerCase()
+if ('yes') {
+  console.log(userName + 'you are correct!'); 
+  alert(userName + 'you are correct!');
+} else {
+  console.log(userName + 'Sorry but I do like spicy food'); 
+  alert(userName + 'Sorry but I do like spicy food');
+}
+
+var questionTwo = prompt(userName + 'Do I The Fast and the Furious Movies? Yes or No'); 
+console.log(userName + 'Do I The Fast and the Furious Movies? Yes or No'); 
+questionTwo.toLowerCase()
+if ('yes') {
+  console.log(userName + 'you are correct!'); 
+  alert(userName + 'you are correct!');
+} else {
+  console.log(userName + 'Awesome! Correct'); 
+  alert(userName + 'Sorry but I do! Even if they are insane!'); 
+}
+
+
+
+var questionThree = prompt(userName + 'Am I in Gex X'); 
+console.log(userName + 'Am I in Gen X'); 
+questionThree.toLowerCase()
+if ('yes') {
+  console.log(userName + 'Sorry! Gen Y forever!'); 
+  alert(userName + 'Sorry! Gen Y forever!');
+} else {
+  console.log(userName + 'You Are Right!'); 
+  alert(userName + 'You Are Right!'); 
+} 
+
+
+var questionFour = prompt(userName + 'Am I a Vegan?'); 
+console.log(userName + 'Am I a Vegan'); 
+questionFour.toLowerCase()
+if ('yes') {
+  console.log(userName + 'Sorry! While I do eat vegan meals I am not vegan!'); 
+  alert(userName + 'Sorry! While I do eat vegan meals I am not vegan!');
+} else {
+  console.log(userName + 'You Are Right!');  
+  alert(userName + 'You Are Right!'); 
+} 
+
+
+
+var questionFive = prompt(userName + 'Have I watched every episode of Dragon Ball Z?'); 
+console.log(userName + 'Have I watched every episode of Dragon Ball Z?'); 
+questionFive.toLowerCase()
+if ('yes') {
+  console.log(userName + 'It/s over 9000!!!! Of Course!');
+  alert(userName + 'It/s over 9000!!!! Of Course!');
+} else {
+  console.log(userName + 'Sorry. I have watched every episode of DBZ!');  
+  alert(userName + 'Sorry. I have watched every episode of DBZ!');  
+}  
+
+var userName = alert('I hope you like my page' + userName);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
