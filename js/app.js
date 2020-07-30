3fe154e69f172c6a633af35aaf0ee364c37b22f1
Henry Funk <georgehenryfunk@gmail.com>
2020-07-29 21:37:23 -0700

    questions 1-5 and 7 function wrap

--- js/app.js
+++ js/app.js
@@ -1,76 +1,86 @@
 'use strict'  
 
+function starWars() {
+  var userName = prompt('Hello, My name is Felicia. What is your name?'); 
+  console.log('Hello' + userName + 'Welcome to my about me page!!!'); 
+  alert('Hello' + userName + 'Welcome to my  about me page!!!'); 
+}
+starWars();
 
-var userName = prompt('Hello, My name is Felicia. What is your name?'); 
-console.log('Hello' + userName + 'Welcome to my about me page!!!'); 
-alert('Hello' + userName + 'Welcome to my  about me page!!!'); 
+function madMax() {
+  var questionOne = prompt(userName  + 'Do I like spicy food? Yes or No'); 
+  console.log(userName + 'Do I like spicy food? Yes or No');  
+  questionOne.toLowerCase() 
+  
+  
+  if (questionOne !=='yes'  && questionOne ==='y'){
+    console.log(userName  + 'you are correct!');
+    alert(userName  + 'you are correct!');
+  } else { (questionOne ==='no' && questionOne ==='n') 
+    console.log(userName  + 'Sorry but I do like spicy food'); 
+    alert(userName + 'Sorry but I do like spicy food');
+  }
+}
+madMax();
 
-
-// var questionOne = prompt(userName  + 'Do I like spicy food? Yes or No'); 
-// console.log(userName + 'Do I like spicy food? Yes or No');  
-// questionOne.toLowerCase() 
-
-
-// if (questionOne !=='yes'  && questionOne ==='y'){
-//   console.log(userName  + 'you are correct!');
-//   alert(userName  + 'you are correct!');
-// } else { (questionOne ==='no' && questionOne ==='n') 
-//   console.log(userName  + 'Sorry but I do like spicy food'); 
-//   alert(userName + 'Sorry but I do like spicy food');
-// }
-
-// var questionTwo = prompt(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
-// console.log(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
-// questionTwo.toLowerCase()
-
-// if (questionTwo !=='yes'  && questionTwo ==='y') {
-//   console.log(userName  + 'you are correct!'); 
-//   alert(userName  + 'you are correct!');
-// } else { (questionTwo ==='no' && questionTwo ==='n')
-//   console.log(userName  + 'Sorry but I do! Even if they are insane!'); 
-//   alert(userName  + 'Sorry but I do! Even if they are insane!'); 
-// }
-
-
-
-// var questionThree = prompt(userName  + 'Am I in Gex X'); 
-// console.log(userName  + 'Am I in Gen X'); 
-// questionThree.toLowerCase()
-
-// if (questionThree !=='yes'  && questionThree ==='y') {
-//   console.log(userName  + 'Sorry! Gen Y forever!'); 
-//   alert(userName + 'Sorry! Gen Y forever!');
-// } else { (questionThree ==='no' && questionThree ==='n')
-//   console.log( userName  + 'You Are Right!'); 
-//   alert(userName  + 'You Are Right!'); 
-// } 
+function it() {
+  var questionTwo = prompt(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
+  console.log(userName  + 'Do I The Fast and the Furious Movies? Yes or No'); 
+  questionTwo.toLowerCase()
+  
+  if (questionTwo !=='yes'  && questionTwo ==='y') {
+    console.log(userName  + 'you are correct!'); 
+    alert(userName  + 'you are correct!');
+  } else { (questionTwo ==='no' && questionTwo ==='n')
+    console.log(userName  + 'Sorry but I do! Even if they are insane!'); 
+    alert(userName  + 'Sorry but I do! Even if they are insane!'); 
+  }
+}
+it();
 
 
-// var questionFour = prompt(userName  + 'Am I a Vegan?'); 
-// console.log(userName  + 'Am I a Vegan'); 
-// questionFour.toLowerCase()
+function avengers() {
+  var questionThree = prompt(userName  + 'Am I in Gex X'); 
+  console.log(userName  + 'Am I in Gen X'); 
+  questionThree.toLowerCase()
+  
+  if (questionThree !=='yes'  && questionThree ==='y') {
+    console.log(userName  + 'Sorry! Gen Y forever!'); 
+    alert(userName + 'Sorry! Gen Y forever!');
+  } else { (questionThree ==='no' && questionThree ==='n')
+    console.log( userName  + 'You Are Right!'); 
+    alert(userName  + 'You Are Right!'); 
+  } 
+}
 
-// if (questionFour !=='yes'  && questionFour ==='y') {
-//   console.log(userName  + 'Sorry! While I do eat vegan meals I am not vegan!'); 
-//   alert(userName  + 'Sorry! While I do eat vegan meals I am not vegan!');
-// } else { (questionFour ==='no' && questionFour ==='n')
-//   console.log(userName  + 'You Are Right!');  
-//   alert(userName  + 'You Are Right!'); 
-// } 
+function joker() {
+  var questionFour = prompt(userName  + 'Am I a Vegan?'); 
+  console.log(userName  + 'Am I a Vegan'); 
+  questionFour.toLowerCase()
+  
+  if (questionFour !=='yes'  && questionFour ==='y') {
+    console.log(userName  + 'Sorry! While I do eat vegan meals I am not vegan!'); 
+    alert(userName  + 'Sorry! While I do eat vegan meals I am not vegan!');
+  } else { (questionFour ==='no' && questionFour ==='n')
+    console.log(userName  + 'You Are Right!');  
+    alert(userName  + 'You Are Right!'); 
+  } 
+}
+joker();
 
-
-
-// var questionFive = prompt(userName  + 'Have I watched every episode of Dragon Ball Z?'); 
-// console.log(userName  + 'Have I watched every episode of Dragon Ball Z?'); 
-// questionFive.toLowerCase()
-
-// if (questionFive ==='yes' && questionFive ==='y') {
-//   console.log(userName  + 'It/s over 9000!!!! Of Course!');
-//   alert(userName  + 'It/s over 9000!!!! Of Course!');
-// } else { (questionFive ==='no' && questionFive ==='n')
-//   console.log(userName  + 'Sorry. I have watched every episode of DBZ!');  
-//   alert(userName + 'Sorry. I have watched every episode of DBZ!');  
-// }  
+function drSleep() {
+  var questionFive = prompt(userName  + 'Have I watched every episode of Dragon Ball Z?'); 
+  console.log(userName  + 'Have I watched every episode of Dragon Ball Z?'); 
+  questionFive.toLowerCase()
+  
+  if (questionFive ==='yes' && questionFive ==='y') {
+    console.log(userName  + 'It/s over 9000!!!! Of Course!');
+    alert(userName  + 'It/s over 9000!!!! Of Course!');
+  } else { (questionFive ==='no' && questionFive ==='n')
+    console.log(userName  + 'Sorry. I have watched every episode of DBZ!');  
+    alert(userName + 'Sorry. I have watched every episode of DBZ!');  
+  }  
+}
 
 function henry() {
   var questionSix = prompt('What number am I thinking of?');
@@ -94,23 +104,26 @@ function henry() {
        alert(userName + 'you are right'); 
 } 
 henry('Hello'); 
-     
-//      var questionSeven =prompt('What is my favorite Star Wars Movie?');
-//      console.log(userName + 'What is my favorite Star Wars Movie?'); 
-//      questionSeven.toLowerCase();
+ 
+function theAmericanizationOfEmily() {
+  var questionSeven =prompt('What is my favorite Star Wars Movie?');
+  console.log(userName + 'What is my favorite Star Wars Movie?'); 
+  questionSeven.toLowerCase();
 
-// for (i = 0; i < 7; i++) {
-// }
-
-
-//      if (questionSeven !== 'The Empire Strikes Back') {
-//       console.log('Sorry'+ userName);
-//       alert('Sorry' + userName);  
-//       prompt =('What is my favorite Star Wars Movie?');
-//       } else { (questionSix === 'The Empire Strikes Back');
-//       console.log(userName +'You got it!!!');
-//       alert(userName + 'You got it!!!');
-    
+for (i = 0; i < 7; i++) {
+}
 
 
-//      var userName = alert('I hope you like my page' +  userName); 
+  if (questionSeven !== 'The Empire Strikes Back') {
+   console.log('Sorry'+ userName);
+   alert('Sorry' + userName);  
+   prompt =('What is my favorite Star Wars Movie?');
+   } else { (questionSix === 'The Empire Strikes Back');
+   console.log(userName +'You got it!!!');
+   alert(userName + 'You got it!!!');
+}
+}
+theAmericanizationOfEmily();
+
+
+var userName = alert('I hope you like my page' +  userName);
